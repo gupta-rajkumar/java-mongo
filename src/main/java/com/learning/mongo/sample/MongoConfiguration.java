d920e3f684f78b0c4cf7903c989afe4c953adaff
Rajkumar Gupta <guptaraj337@gmail.com>
2025-11-29 14:19:40 +0530

    2nd commit

--- src/main/java/com/learning/mongo/sample/MongoConfiguration.java
+++ src/main/java/com/learning/mongo/sample/MongoConfiguration.java
@@ -4,12 +4,12 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.Conventions;
 import org.bson.codecs.pojo.PojoCodecProvider;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.List;
 
@@ -18,30 +18,21 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @Configuration
-public class MongoConfiguration implements InitializingBean {
-    private static final String PACKAGE = "com.learning.mongo.sample.model";
-    private static final String URI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
-    private MongoClient client;
-    private static final PojoCodecProvider POJO_CODEC_PROVIDER = PojoCodecProvider
-            .builder()
-            .register(PACKAGE)
-            .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
-            .build();
-    private static final CodecRegistry CODEC_REGISTRY = fromRegistries(
-            getDefaultCodecRegistry(),
-            fromProviders(POJO_CODEC_PROVIDER));
+@PropertySource("classpath:application.properties")
+public class MongoConfiguration {
+    @Value("${com.sample.mongo.model.path}")
+    private String modelClasses;
+    @Value("${com.sample.mongo.uri}")
+    private String uri;
 
     @Bean
     public MongoClient getClient() {
-        return client;
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        client = MongoClients.create(
-                MongoClientSettings.builder()
-                        .applyConnectionString(new ConnectionString(URI))
-                        .codecRegistry(CODEC_REGISTRY)
+        return MongoClients.create(
+                MongoClientSettings.builder().applyConnectionString(new ConnectionString(uri)).codecRegistry(fromRegistries(getDefaultCodecRegistry(),
+                                fromProviders(PojoCodecProvider.builder().register(modelClasses)
+                                        .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
+                                        .build())))
                         .build());
     }
+
 }
